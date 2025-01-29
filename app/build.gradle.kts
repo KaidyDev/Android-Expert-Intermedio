37bbee73c960c68b1e4765d1984be72619978bf9
Victor Adrián Dominguez Garcia <gtmcpinwii@gmail.com>
2025-01-28 22:01:10 -0600

    permissions management

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -55,6 +55,14 @@ android {
 
 dependencies {
 
+    //Camera X
+    implementation (libs.androidx.camera.core)
+    implementation (libs.androidx.camera.camera2)
+    implementation (libs.androidx.camera.lifecycle)
+    implementation (libs.androidx.camera.view)
+    implementation (libs.androidx.camera.extensions)
+
+
     //NavComponent
     implementation(libs.dagger.hilt.android)
     kapt(libs.dagger.hilt.compiler)
